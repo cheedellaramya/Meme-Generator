a2bcdee3354915f4e2b688889c44d88fff737aae
Ramya Krishna <cheedellaramya@gmail.com>
2023-03-03 20:52:28 +0530

    initial-commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className='header'>
+        
+          <img className='header-image' src='https://media.tenor.com/KNpiKw5Y2lkAAAAi/sagithegavar-pc.gif' alt='loading...'/>
+          <h1 className='header-title'>Meme Generator</h1>
+        <h4 className='header-project'>React Course - Project 3</h4>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
