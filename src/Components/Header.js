c490652b042203e44bd5992be21348b858a00ba7
Ramya Krishna <cheedellaramya@gmail.com>
2023-03-06 15:40:43 +0530

    icon path Changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import logo from '../assets/images/meme-image.jpg'
 const Header = () => {
   return (
     <div>
       <header className='header'>
         
-          <img className='header-image' src='https://media.tenor.com/KNpiKw5Y2lkAAAAi/sagithegavar-pc.gif' alt='loading...'/>
+          <img className='header-image' src={logo} alt='loading...'/>
           <h1 className='header-title'>Meme Generator</h1>
         <h4 className='header-project'>React Course - Project 3</h4>
       </header>
--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,77 +1,69 @@
-
-import axios from "axios"
-import React from "react"
-import { useState,useEffect } from "react"
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Meme() {
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemes, setAllMemes] = useState([])
-    
-//     useEffect(() => {
-//         fetch("https://api.imgflip.com/get_memes")
-//         .then(res =>res.json())
-//         .then((data) => setAllMemes(data.data.memes))
-//   }, [])
-useEffect(()=>{
-    axios.get("https://api.imgflip.com/get_memes").then((res)=>{
-        setAllMemes(res.data.data.memes)})
-},[])
-    
-    function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
-        // setMeme(prevMeme => ({
-        //     ...prevMeme,
-        //     randomImage: url
-        // }))
-        setMeme({topText:'',bottomText:'', randomImage: url})
-    }
-    
-    function handleChange(event) {
-        const {name, value} = event.target
-        setMeme({...meme, [name]: value})
-    }
-    
-    return (
-        <main>
-            <div className="form">
-                <div>
-                    <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form-input"
-                    value={meme.topText}
-                    onChange={handleChange}
-                    name="topText"
-                    />
-                </div>
-                <div>
-                    <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form-input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                    />
-                </div>
-               
-            </div>
-            <button 
-                    className="form-button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme-image" alt='loading...'/>
-                <h2 className="meme-text top">{meme.topText}</h2>
-                <h2 className="meme-text bottom">{meme.bottomText}</h2>
-            </div>
-        </main>
-    )
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = useState([]);
+
+  //     useEffect(() => {
+  //         fetch("https://api.imgflip.com/get_memes")
+  //         .then(res =>res.json())
+  //         .then((data) => setAllMemes(data.data.memes))
+  //   }, [])
+  useEffect(() => {
+    axios.get("https://api.imgflip.com/get_memes").then((res) => {
+      setAllMemes(res.data.data.memes);
+    });
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme({ topText: "", bottomText: "", randomImage: url });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme({ ...meme, [name]: value });
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <div>
+          <input
+            type="text"
+            placeholder="Top text"
+            className="form-input"
+            value={meme.topText}
+            onChange={handleChange}
+            name="topText"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form-input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <button className="form-button" onClick={getMemeImage}>
+        Get a new meme image 🖼
+      </button>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme-image" alt="loading..." />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
 }
